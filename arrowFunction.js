99af54603c8726c6561c6436f02625070e099eb3
purteeek <deshpandepratikr@gmail.com>
2017-07-08 12:22:57 +0530

    Added multiple params arrow functions.

--- arrowFunction.js
+++ arrowFunction.js
@@ -16,3 +16,9 @@ console.log((getpriceES6()))
 const getPriceES6 = discount => 3.15 * discount
 
 console.log(getPriceES6(0.6))
+
+//Multiple parameterised arrow functions in ES6
+
+const getPriceES6MultipleParams = (discount,tax) => 3.15 * discount + tax
+console.log(getPriceES6MultipleParams(0.20,1))
+
