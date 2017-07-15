685721d20abd254d4634c25e58a264744cd37ddc
purteeek <deshpandepratikr@gmail.com>
2017-07-15 23:35:40 +0530

    Added some basic info about objects in memory

---
+++ Understanding-Weird-Parts-of-Javascript/3. Objects and Functions/3.1 Objects - Intro /app.js
@@ -0,0 +1,11 @@
+// Objects are collection of names and value pairs. 
+// That is an object .
+// What can objects have inside them?
+// Anything 
+// objects have properties and methods.
+// a primitive type (out of those 6) is called a property
+// another object (this object I am talking about is inside a certain object). is also a property
+// if objects have functions in them they are called as methods 
+// So objects reside in the memory and they contain references to the properties and the methods which are also 
+// present in the memory .
+// Fair enough?
