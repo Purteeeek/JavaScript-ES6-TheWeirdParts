9274d0d763162ad7ead8e199d0b46c36c4f0b260
purteeek <deshpandepratikr@gmail.com>
2017-07-15 18:54:26 +0530

    Added definition of associativity and precendence

---
+++ Understanding-Weird-Parts-of-Javascript/2. Types and Operators/2.3 Operator Precedence and Associativity/app.js
@@ -0,0 +1,7 @@
+// PRECEDENCE :
+// Precedence is nothing but what operator function gets executed first when there 
+// are multiple operator function in one line
+
+//ASSOCIATIVITY:
+// What order operator functions get called in : Left to right or right to left
+// WHen Operator functions have same precedence.
