2e27c50868a6d4b05aa3228995814c8bbbc510af
purteeek <deshpandepratikr@gmail.com>
2017-07-15 11:01:55 +0530

    Added the explanation for the Functions Context and Variable Environments

--- Understanding-Weird-Parts-of-Javascript/Functions,Context/app.js
+++ Understanding-Weird-Parts-of-Javascript/Functions,Context/app.js
@@ -13,3 +13,25 @@ var myVar = 1
 console.log(myVar)
 a();
 console.log(myVar)
+
+//What will this code do?
+//The output is
+//1
+//2
+//undefined 
+//1
+// SO Javascript makes the first global execution context
+// the window object and in that context the value of 
+// myVar is 1
+// now when a() is invoked a new execution context is created
+// which is on top of the window object. (The execution stack)
+// and in that context for a the value of myVar is 2
+// now when b() got invoked a third execution context was created 
+// and in the third execution context the value of myVar was undefined
+// After the execution of b got over the context was popped from the 
+// execution stack and similar happened with a. and then finally again
+// globally the value of myVar is 1
+
+//------SCOPE---------
+// This tells us a lot about the scope of the variables 
+// and how there can be different values at different points of time
