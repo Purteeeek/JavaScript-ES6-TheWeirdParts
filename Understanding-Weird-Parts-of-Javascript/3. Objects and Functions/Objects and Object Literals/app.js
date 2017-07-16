ba37e0a71837093290909ebd1e95143f7667b939
purteeek <deshpandepratikr@gmail.com>
2017-07-16 11:37:11 +0530

    Added a note about mix and match of literals and dot

--- Understanding-Weird-Parts-of-Javascript/3. Objects and Functions/Objects and Object Literals/app.js
+++ Understanding-Weird-Parts-of-Javascript/3. Objects and Functions/Objects and Object Literals/app.js
@@ -36,3 +36,18 @@ greetMe({
 
 // here we called the function greetMe and created an object on the fly while calling the function
 // might look weird at first but yeah it is possible in Javascript
+
+// YOu can also change a property or add a new property on the fly with dot operator and our object 
+//literal
+
+person1.contact = {
+	mobile : '76786e78638768',
+	landline : '2121323213'
+}
+
+
+console.log(person1)
+//so you can mix and match and stuff still works. because JS engine works the same way no matter 
+// what syntax youre using in this way.
+
+// We use object literals because theyre powerful and make the code easy to read
