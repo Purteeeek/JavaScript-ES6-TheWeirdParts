b2beb274dc802a599fac75870b7ea06a7e314c4d
purteeek <deshpandepratikr@gmail.com>
2017-07-16 13:09:39 +0530

    Explained about JSON and Object literals

--- Understanding-Weird-Parts-of-Javascript/3. Objects and Functions/3.4 JSON and Object Literals/app.js
+++ Understanding-Weird-Parts-of-Javascript/3. Objects and Functions/3.4 JSON and Object Literals/app.js
@@ -16,8 +16,24 @@ console.log(objectLiteral) // logs the above object
 // The thing below is JSON . the difference being in JSON the properties have to wrapped around in 
 // double quotes. Any thing that is JSON is Javascript object literal syntax.
 // but not all javascript literal syntax is JSON.
-// Json comes with strict rules
-{
-	"firstname" : "Pratik" , 
-	"isAProgrammer" : true 
-}
+// Json comes with strict rules 
+//
+// 		{
+//			"firstname" : "Pratik" , 
+//			"isAProgrammer" : true 
+// 		}
+
+// JSON is a part of javascript that got widely accepted as the means to send data over the net
+
+// there are some cool inbuilt things you can do with it.
+
+console.log(JSON.stringify(objectLiteral))
+// JSON.stringify will convert the object literal into JSON 
+// check what it logs
+
+// similarly you can also create an object from Json lets take the JSON mentioned above and make an
+// object 
+
+var jsonValue = JSON.parse('{"firstname" : "Pratik" , "isAProgrammer" : true }')
+// I took a string which is proper JSON and converted to an object.
+console.log(jsonValue)
