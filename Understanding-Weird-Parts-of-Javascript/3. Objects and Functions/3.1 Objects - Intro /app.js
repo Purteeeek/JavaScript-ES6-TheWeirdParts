a605180455efa3a5788aa6fb59c8029b0c0acd1e
purteeek <deshpandepratikr@gmail.com>
2017-07-16 11:01:16 +0530

    Added code for understanding dot operator in javascript

--- Understanding-Weird-Parts-of-Javascript/3. Objects and Functions/3.1 Objects - Intro /app.js
+++ Understanding-Weird-Parts-of-Javascript/3. Objects and Functions/3.1 Objects - Intro /app.js
@@ -21,6 +21,29 @@ person['lastname'] = "Deshpande";
 
 var personfirstname = 'firstname'
 
-console.log(person) // will log the entire object. 
-console.log(person[personfirstname]) // will log Pratik
+console.log(person) ;// will log the entire object. 
+console.log(person[personfirstname]) ;// will log Pratik
 
+// The another way to do the same thing is the . operator , which is easy to the eyes
+
+console.log(person.firstname) ; // Pratik
+console.log(person.lastname) ; // Deshpande
+
+// . is also an operator. Check MDN. 
+// So what the . is doing is it is finding firstname inside person and returning it .
+// now lets add more properties to our person
+
+address = new Object();
+person.address.street = "12 Main Street"
+person.address.city = "Bombay"
+person.address.country = "India"
+
+// in the above statements we have created other properties inside the address object of our person object
+// and set the values.
+
+console.log(person.address.city); // Bombay
+console.log(person["address"]["city"]); // Bombay
+
+// The above are just two different ways of doing the same thing . 
+// one with [] operator and the other with . operator.
+// dot operator is the preferred one though as it makes the code more readable and easier to debug for a hooman. 
