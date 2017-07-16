4405841265e71d14a51b31c1c48f1fd79b2d1613
purteeek <deshpandepratikr@gmail.com>
2017-07-16 11:33:13 +0530

    passed object as the parameter

--- Understanding-Weird-Parts-of-Javascript/3. Objects and Functions/Objects and Object Literals/app.js
+++ Understanding-Weird-Parts-of-Javascript/3. Objects and Functions/Objects and Object Literals/app.js
@@ -19,3 +19,20 @@ console.log(person1) // You will get the object with all those properties
 
 // So we can initialise all the name and value pairs we want just like that.
 // This is the preferred way I was talking about. 
+
+function greetMe(person) {
+	console.log("Hey There " + person.firstname)
+}
+
+function greetMe(person1) // Hey There Pratik 
+// you passed object as a parameter.
+
+// but guess whats better you can create objects on the fly like we make our variables while 
+
+greetMe({
+	firstname: 'Abhishek',
+	lastname: 'Jain'
+})
+
+// here we called the function greetMe and created an object on the fly while calling the function
+// might look weird at first but yeah it is possible in Javascript
