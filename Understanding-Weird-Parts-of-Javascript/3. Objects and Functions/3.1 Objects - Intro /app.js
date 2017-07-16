932a9b26347a1b57c79e062dd48125d918eb5f9e
purteeek <deshpandepratikr@gmail.com>
2017-07-16 10:52:55 +0530

    Added about the [] operator for objects

--- Understanding-Weird-Parts-of-Javascript/3. Objects and Functions/3.1 Objects - Intro /app.js
+++ Understanding-Weird-Parts-of-Javascript/3. Objects and Functions/3.1 Objects - Intro /app.js
@@ -9,3 +9,18 @@
 // So objects reside in the memory and they contain references to the properties and the methods which are also 
 // present in the memory .
 // Fair enough?
+
+var person = new Object(); // Note : This is not the preferred way of making an object but 
+//I am doing this just because I want to show you that this is one of the way before 
+// actually even using the preferred one
+
+person['firstname'] = 'Pratik';
+// So we have added the first key value pair in our object. 
+//person[key] = value;
+person['lastname'] = "Deshpande";
+
+var personfirstname = 'firstname'
+
+console.log(person) // will log the entire object. 
+console.log(person[personfirstname]) // will log Pratik
+
