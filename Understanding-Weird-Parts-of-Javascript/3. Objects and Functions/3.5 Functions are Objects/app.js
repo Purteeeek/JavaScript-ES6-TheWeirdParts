989fd0d7f30f74f6f98ac263bc680289c9daec48
purteeek <deshpandepratikr@gmail.com>
2017-07-16 20:51:57 +0530

    Added some explanation for function are objects

--- Understanding-Weird-Parts-of-Javascript/3. Objects and Functions/3.5 Functions are Objects/app.js
+++ Understanding-Weird-Parts-of-Javascript/3. Objects and Functions/3.5 Functions are Objects/app.js
@@ -3,3 +3,13 @@
 // meaning everything you can do with other types you can do it with a function.
 // assign them to variables, pass them around, create them on the fly 
 //these change the way we code
+
+// Function is a special type of an object though.
+// it has all the properties of an object plus some extra properties.
+// The function can have a primitive, other functions, other objects attached to it.
+// whats special about this function object is.
+// there is a special property called the code. which is invokable.
+// yes the code you and I write in a function is merely one of the many properties which is invokable
+// also it is optional to give a name to a function in js. you can have anonymous functions too
+
+// This is a very weird concept. so lets see and prove it 
