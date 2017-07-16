066094e406624cd5061f686cd153ebf24b5ec023
purteeek <deshpandepratikr@gmail.com>
2017-07-16 20:46:40 +0530

    Added explanation for first class functions

---
+++ Understanding-Weird-Parts-of-Javascript/3. Objects and Functions/3.5 Functions are Objects/app.js
@@ -0,0 +1,5 @@
+// Yes functions are objects
+// javascript has first class functions.
+// meaning everything you can do with other types you can do it with a function.
+// assign them to variables, pass them around, create them on the fly 
+//these change the way we code
