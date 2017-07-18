69dfb3d58d76d9430dd0ffe49b7815a5db6e53ed
purteeek <deshpandepratikr@gmail.com>
2017-07-19 00:54:56 +0530

    Added an example for by reference

--- Understanding-Weird-Parts-of-Javascript/3. Objects and Functions/3.7 Value and Reference/app.js
+++ Understanding-Weird-Parts-of-Javascript/3. Objects and Functions/3.7 Value and Reference/app.js
@@ -14,3 +14,26 @@ console.log(b); // 2
 // this is called by value.
 // and thats the reason why, when a was changed b did not go through any change.
 // this is followed with primitives in javascript
+
+// By Reference.
+
+// With objects and functions. the case is different. 
+// lets see this. 
+
+var c = { name : "Pratik" }
+var d;
+d = c ;
+c.name = "Abhishek";
+console.log(c);
+console.log(d);
+
+// You will be surprised that in both c and d the name property was changed to Abhishek, where as you might
+// have expected Pratik in d.
+// What happens during the case of objects is.
+// when d = c was executed
+// no new memory space was set up and the content was not copied.
+// instead, d also pointed at the same memory location 
+// this is like you having two names, an alias.
+// because as both the things are pointing to the same location, the content change will be reflected .
+// c and d are just two different names for the same object in this case. 
+
