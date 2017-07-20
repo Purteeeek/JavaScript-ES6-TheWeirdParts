d9044e0795af09120d6b767c4ca3729ae3033ac2
purteeek <deshpandepratikr@gmail.com>
2017-07-20 12:39:29 +0530

    Added some examples for functions invoked at a global level and this

--- Understanding-Weird-Parts-of-Javascript/3. Objects and Functions/3.8 Objects, Functions and this/app.js
+++ Understanding-Weird-Parts-of-Javascript/3. Objects and Functions/3.8 Objects, Functions and this/app.js
@@ -6,3 +6,29 @@
 // helps us to travel the scope chain.
 // and what we also know that we get a variable called this.
 // Now this will point to different things depending on how the function was invoked 
+
+console.log(this) // this points to the window object.
+ 
+function a(){
+	console.log(this);
+}
+
+a()
+// surprised much?
+// this still refers to the window object even if it is inside a function.
+// so whenever you are invoking a function at a global level it is going to point at the window object.
+
+var b = function () {
+	console.log(this)
+	this.myVariable = "Hello"
+}
+
+b() 
+console.log(myVariable)
+
+// so even with the function expression, the behaviour of this doesnt change. 
+// the function was invoked on a global level and this still points to the window object.
+// now you can see that even after b was invoked and completed. we can still print myVariable.
+// because we attached it to the global ie the window object and not the function.
+// we have three execution contexts, global ,a and, b and all the three this are pointing towards the same window 
+// object
