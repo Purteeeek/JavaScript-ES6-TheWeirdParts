638acedfdcf4878644f6657552c8b6cd7f92d066
purteeek <deshpandepratikr@gmail.com>
2017-07-20 20:23:17 +0530

    added explanation for the example

--- Understanding-Weird-Parts-of-Javascript/3. Objects and Functions/3.15 Closures - 2/app.js
+++ Understanding-Weird-Parts-of-Javascript/3. Objects and Functions/3.15 Closures - 2/app.js
@@ -3,7 +3,8 @@ function buildfunc() {
 
 	for(var i = 0; i < 3 ; i++){
 		arr.push( function() {
-
+			   // var x = i
+			   // console.log(x)
 				console.log(i);
 			}
 		)
@@ -17,3 +18,31 @@ var fz = buildfunc();
 fz[0]();
 fz[1]();
 fz[2]();
+
+// we created a function that returns us array of three functions.
+// remember that its returning a function.
+// so what did you expect to console.log when the three functions were called
+// 0 1 2?
+// what was the output?
+// 3
+// 3
+// 3
+
+// surprised?
+// want to know what happened?
+// so when the three functions were created i was different. but all the three functions are refering to 
+// the same variable i 
+// which is inside buildfunc()
+// now that we have studied closures we understand that
+// even when the execution context of buildfunc() the memory space and the variables in the space
+// were there and all the three functions have their outer reference to them
+// now when you are actually calling the function the value i that they are accessing is 3
+// even though the when the functions were being created the value of i was different
+// but they are still accessing the same variable i
+// now the functions in the array are all the same
+// and the i is refering to the same space
+// and also i when build function was completed was 3
+// and therefore all the three printed 3
+// understanding closures?
+// uncomment those two lines in the function and refresh the browser you'll understand all that I typed in a 
+// better way
