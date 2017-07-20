58f7a9274983adc9e4e70dd7ce7034ca320eca63
purteeek <deshpandepratikr@gmail.com>
2017-07-20 14:17:32 +0530

    Explaining function call without param call and how hoisting gives no error

--- Understanding-Weird-Parts-of-Javascript/3. Objects and Functions/3.10 Arguments/app.js
+++ Understanding-Weird-Parts-of-Javascript/3. Objects and Functions/3.10 Arguments/app.js
@@ -11,3 +11,24 @@
 // what does this variable hold?
 // it holds all the parameters you pass to the function while calling it.
 // whatever we put inside those parantheses while calling the function are stored by the variable arguments
+
+function sayHello(firstName, lastName, language){
+	console.log(firstName);
+	console.log(lastName);
+	console.log(language);
+	console.log("------------------");
+}
+
+sayHello();
+// in many programming languages this will be an error but javascript doesnt care if you pass the params 
+// or not, if you dont pass them theyre simply going to be undefined.
+// remember hoisting?
+// the variable space for those variables was set to undefined when execution context was being created.
+// and thats why if you dont pass them they are gonna remain undefined.
+
+sayHello("Pratik");
+sayHello("Pratik", "Deshpande");
+sayHello("Pratik","Deshpande","English");
+
+// the above calls prove that whe you pass only one param it goes in the left most param.
+// that means the params get the value from left to right. ie how the programmer wrote it.
