3d275b064202b196942ea8ecd455c198f85254ff
purteeek <deshpandepratikr@gmail.com>
2017-07-20 20:47:56 +0530

    used IIFE's to solve what is the common bug of js

--- Understanding-Weird-Parts-of-Javascript/3. Objects and Functions/3.15 Closures - 2/app.js
+++ Understanding-Weird-Parts-of-Javascript/3. Objects and Functions/3.15 Closures - 2/app.js
@@ -44,5 +44,67 @@ fz[2]();
 // THE LAST VALUE OF i was 3
 // and therefore all the three printed 3
 // understanding closures?
-// uncomment those two lines in the function and refresh the browser you'll understand all that I typed in a 
-// better way
+
+
+// So now what do we do if we actually want console.log to be
+// 0
+// 1
+// 2
+// what will we do?
+// somehow we will need three different execution context for the three variables.
+// Remember our friend IIFE's?
+// they will help us achieve the behaviour
+
+function buildfunc1() {
+	var arr = [];
+
+	for(var i = 0; i < 3 ; i++){
+		arr.push( 
+			(function(j) {
+				return function(){
+					console.log(j)
+					console.log("----------------------")
+				}
+			}(i))
+		)
+	}
+
+	return arr ;
+}
+
+var fz1 = buildfunc1();
+
+fz1[0]();
+fz1[1]();
+fz1[2]();
+
+// so what is happening now?
+// what we are doing is 
+// we created an IIFE which returns functions.
+// now the IIFE will be run thrice.
+// so three execution contexts will be created where the value of j would be 
+// 0
+// 1
+// 2 
+// and in the iife we passed the parameter i which is 0 1 2
+// even if the function is out of the execution stack 
+// we know closures
+// the functions inside iife were created and they still have outer reference to the execution 
+// context of IIFE
+// so three execution contexts of three IIFes
+// and the three functions in our array have three different outward reference
+// and now all the functions are printing j 
+// and there are three j's
+// the first function which has an outer reference to exec context of first IIFE  has j to 0
+// the second function which has an outer reference to exec context of second IIFE  has j to 1
+// the third function which has an outer reference to exec context of third IIFE  has j to 2
+
+// hence what you got was 
+// 0
+// 1 
+// 2
+
+// so now I hope these examples give you a better understanding of closures.
+// write your examples
+// play with the code
+// this concept will sink in
