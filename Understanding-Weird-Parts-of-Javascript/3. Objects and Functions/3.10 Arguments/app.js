46c0ad878aa311bab9e432619e600343820e498d
purteeek <deshpandepratikr@gmail.com>
2017-07-20 14:09:04 +0530

    Added the definition and explanation for what arguments are

---
+++ Understanding-Weird-Parts-of-Javascript/3. Objects and Functions/3.10 Arguments/app.js
@@ -0,0 +1,13 @@
+// arguments and Spread.
+// The later version of javascript has Spread.
+// so we wont use arguments as much as we do .
+// but lets talk about what arguments is.
+// Remember?
+// When you call a function the execution context is being set up and 
+// javascript is giving you certain things,
+// like the variable environment , the reference to the outer environment (the scope chain), the this keyword
+// so whenever a function is being called javascript gives you one more variable.
+// arguments
+// what does this variable hold?
+// it holds all the parameters you pass to the function while calling it.
+// whatever we put inside those parantheses while calling the function are stored by the variable arguments
